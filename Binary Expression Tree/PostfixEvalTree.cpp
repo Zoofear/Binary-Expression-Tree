98def289733d6361665ae86a055366c1d28892ec
Zoofear <misterz0312@gmail.com>
2025-05-08 14:58:00 -0400

    Working on the building and functions of the tree

--- Binary Expression Tree/PostfixEvalTree.cpp
+++ Binary Expression Tree/PostfixEvalTree.cpp
@@ -1,6 +1,21 @@
 #include "PostfixEvalTree.h"
 
-void PostfixEval::createPostfixTree()
+void PostfixEval::createPostfixTree(node* root)
 {
-
+	if (!ss)
+	{
+		return;
+	}
+	if (std::isdigit(token[0]) || (token[0] == '-' || token[0] == '.') && token.length() > 1)
+	{
+		root->data = token;
+		root->left = nullptr;
+		root->right = nullptr;
+	}
+	else if (token == "^" || token == "+" || token == "-" || token == "*" || token == "/")
+	{
+		root->data = token;
+		createPostfixTree(root->left);
+		createPostfixTree(root->right);
+	}
 }
--- Binary Expression Tree/PostfixEvalTree.h
+++ Binary Expression Tree/PostfixEvalTree.h
@@ -21,17 +21,28 @@ class PostfixEval
 {
 public:
 
-	PostfixEval(std::string input):root(nullptr), start(input){} //constructor that initializes the tree by setting a root node to nullptr
+	PostfixEval(std::string input) :root(nullptr), start(input) { tokenizer(); } //constructor that initializes the tree by setting a root node to nullptr
 	
-	void createPostfixTree();	//Creates a binary expression tree based off of the postfix expression stored in the string start.
+	void createPostfixTree()					//Function overload
+	{
+		createPostfixTree(root);
+	}
+
+	void tokenizer()						//Creates the list of tokens to use.
+	{
+		std::istringstream ss(start);
+	}
 
 	void deleteTree();			//Deletes the tree and sets the root back to nullptr
 
 	bool isTreeEmpty();			//Returns true if tree is full, false if it is empty
 
-	std::string infixTraversal();	//returns a expression in infix form by traversing through the tree in order
+	void infixTraversal()
+	{
+		infixTraversal(root);
+	}
 	
-	std::string postFixTraversal();	//returns an expression in postfix form by traversing through the tree in post order.
+	void postFixTraversal();	//returns an expression in postfix form by traversing through the tree in post order.
 
 	double evaluate(std::string x, double left, double right);	//Used to evaluate the operands and operator and return it as a value
 
@@ -39,11 +50,17 @@ public:
 
 
 private:
+	
+	void createPostfixTree(node* root);		//Creates a binary expression tree based off of the postfix expression stored in the string start.;	//Function Overload
 
+	void infixTraversal(node* root); //returns a expression in infix form by traversing through the tree in order
+
+	int stringstreamiterator = 0;
+	std::istringstream ss;
 	std::string start;
 	double output;
 	std::string token;
-	std::stack<std::string> temp;
+	std::stack<std::string> tempstack;
 	node* root;
 
 };
